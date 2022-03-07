231257d3ea6fc3ba6b25d557040341a90b80feef
Justin Wada <61168915+jtomowada27@users.noreply.github.com>
2022-03-06 17:49:08 -0700

    Add files via upload

---
+++ Project2/lexer.cc
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) Rida Bazzi, 2016
+ *
+ * Do not share this file with anyone
+ */
+#include <iostream>
+#include <istream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+#include "lexer.h"
+#include "inputbuf.h"
+#include "parser.h"
+
+using namespace std;
+
+string reserved[] = { "END_OF_FILE","PUBLIC", "PRIAVTE", "EQUAL",
+"COLON", "SEMICOLON", "LBRACE", "RBRACE", "ID", "ERROR"
+// TODO: Add labels for new token types here (as string)
+};
+
+#define KEYWORDS_COUNT 5
+string keyword[] = {"public", "private" };
+
+void Token::Print()
+{
+    cout << "{" << this->lexeme << " , "
+         << reserved[(int) this->token_type] << " , "
+         << this->line_no << "}\n";
+}
+
+LexicalAnalyzer::LexicalAnalyzer()
+{
+    this->line_no = 1;
+    tmp.lexeme = "";
+    tmp.line_no = 1;
+    tmp.token_type = ERROR;
+}
+
+Token LexicalAnalyzer::SkipComment()
+{
+char slash;
+input.GetChar(slash);
+    if(input.EndOfInput())
+    {
+    input.UngetChar(slash);
+    }
+    else if(slash != '\n')
+    {
+        while(slash != '\n')
+        {
+        input.GetChar(slash);
+        line_no += 1;
+        }   
+    }
+    else
+    {
+    input.UngetChar(slash);
+    }
+    return GetToken();
+}
+
+bool LexicalAnalyzer::SkipSpace()
+{
+    char c;
+    bool space_encountered = false;
+
+    input.GetChar(c);
+    line_no += (c == '\n');
+
+    while (!input.EndOfInput() && isspace(c)) {
+        space_encountered = true;
+        input.GetChar(c);
+        line_no += (c == '\n');
+    }
+
+    if (!input.EndOfInput()) {
+        input.UngetChar(c);
+    }
+    return space_encountered;
+}
+
+bool LexicalAnalyzer::IsKeyword(string s)
+{
+    for (int i = 0; i < KEYWORDS_COUNT; i++) {
+        if (s == keyword[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+TokenType LexicalAnalyzer::FindKeywordIndex(string s)
+{
+    for (int i = 0; i < KEYWORDS_COUNT; i++) {
+        if (s == keyword[i]) {
+            return (TokenType) (i + 1);
+        }
+    }
+    return ERROR;
+}
+
+Token LexicalAnalyzer::ScanIdOrKeyword()
+{
+    char c;
+    input.GetChar(c);
+
+    if (isalpha(c)) {
+        tmp.lexeme = "";
+        while (!input.EndOfInput() && isalnum(c)) {
+            tmp.lexeme += c;
+            input.GetChar(c);
+        }
+        if (!input.EndOfInput()) {
+            input.UngetChar(c);
+        }
+        tmp.line_no = line_no;
+        if (IsKeyword(tmp.lexeme))
+            tmp.token_type = FindKeywordIndex(tmp.lexeme);
+        else
+            tmp.token_type = ID;
+    } else {
+        if (!input.EndOfInput()) {
+            input.UngetChar(c);
+        }
+        tmp.lexeme = "";
+        tmp.token_type = ERROR;
+    }
+    return tmp;
+}
+
+// you should unget tokens in the reverse order in which they
+// are obtained. If you execute
+//
+//    t1 = lexer.GetToken();
+//    t2 = lexer.GetToken();
+//    t3 = lexer.GetToken();
+//
+// in this order, you should execute
+//
+//    lexer.UngetToken(t3);
+//    lexer.UngetToken(t2);
+//    lexer.UngetToken(t1);
+//
+// if you want to unget all three tokens. Note that it does not
+// make sense to unget t1 without first ungetting t2 and t3
+//
+TokenType LexicalAnalyzer::UngetToken(Token tok)
+{
+    tokens.push_back(tok);;
+    return tok.token_type;
+}
+
+Token LexicalAnalyzer::GetToken()
+{
+    char c;
+
+    // if there are tokens that were previously
+    // stored due to UngetToken(), pop a token and
+    // return it without reading from input
+    if (!tokens.empty()) {
+        tmp = tokens.back();
+        tokens.pop_back();
+        return tmp;
+    }
+
+    SkipSpace();
+    tmp.lexeme = "";
+    tmp.line_no = line_no;
+    input.GetChar(c);
+    switch (c) {
+        case 'p':
+            char ur,y;
+            input.GetChar(ur);//r or u for pRivate or pUblic respectively
+            if(ur == 'u')
+            {
+            tmp.token_type = PUBLIC;
+            input.GetChar(ur);input.GetChar(ur);input.GetChar(ur);input.GetChar(ur);
+            return tmp;
+            }
+            if(ur == 'r')
+            {
+            tmp.token_type = PRIVATE;
+            input.GetChar(ur);input.GetChar(ur);input.GetChar(ur);input.GetChar(ur);input.GetChar(ur);
+            return tmp;
+            }
+            input.UngetChar(ur);
+        case '=':
+            tmp.token_type = EQUAL;
+            return tmp;
+        case ':':
+            tmp.token_type = COLON;
+            return tmp;
+        case ',':
+            tmp.token_type = COMMA;
+            return tmp;
+        case ';':
+            tmp.token_type = SEMICOLON;
+            return tmp;
+        case '{':
+            tmp.token_type = LBRACE;
+            return tmp;
+        case '}':
+            tmp.token_type = RBRACE;
+            return tmp;
+        case '/':
+            char slash;
+            input.GetChar(slash);
+            if(slash == '/')
+            {
+            return SkipComment();
+            }
+            else{input.UngetChar(slash);}
+        default:
+            if (isdigit(c)) {
+                input.UngetChar(c);
+                //return ScanNumber();
+            } else if (isalpha(c)) {
+                input.UngetChar(c);
+                return ScanIdOrKeyword();
+            } else if (input.EndOfInput())
+                tmp.token_type = END_OF_FILE;
+            else
+                tmp.token_type = ERROR;
+
+            return tmp;
+    }
+}
+int main()
+{
+    Parser parser;
+    parser.list = new struct scopeList();
+    parser.parse_program();
+    //return 0;//may not need it
+}
